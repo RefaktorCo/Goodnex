8836a6dfdc9e49e2697b1b9708af1ef3d4aace88
refaktorco <refaktor.co@gmail.com>
2013-08-27 09:23:07 -0700

    -Contact form finished, time for third page and color picker for boxes.

--- js/goodnex.js
+++ js/goodnex.js
@@ -1,5 +1,6 @@
 jQuery(document).ready(function ($) {
 
   $(".comment-form .form-actions input[type='submit']").addClass("button default");
+  $(".contact-form .form-actions input[type='submit']").addClass("button default");
 
 });
