fd91178250948a9a7e70b9a7e68018834712501b
refaktorco <refaktor.co@gmail.com>
2013-09-05 16:26:40 -0700

    -Changelog, updated docs, added URL for detail box. I think this is done.

--- templates/node/node--detail-box.tpl.php
+++ templates/node/node--detail-box.tpl.php
@@ -1,21 +1,21 @@
 <div class="four columns">
-
-	<div class="detail-box">
-
-		<div class="detail-entry">
-
-			<i class="detail-icon <?php print render($content['field_detail_icon']); ?>"></i>
-
-			<h5><?php print render($content['field_detail_caption']); ?></h5>
-
-			<p>
-				<?php print render($content['field_detail_info']); ?>
-			</p>
-
-		</div><!--/ .detail-entry-->
-
-		<div data-color-state="<?php if (render($content['field_detail_color'])): ?>#<?php print render($content['field_detail_color']); ?> <?php endif; ?>" data-color-hover="" class="transform"></div>
-
-	</div><!--/ .detail-box-->
-
+  <a target="_self" href="<?php print render($content['field_detail_url']); ?>">
+		<div class="detail-box">
+	
+			<div class="detail-entry">
+	
+				<i class="detail-icon <?php print render($content['field_detail_icon']); ?>"></i>
+	
+				<h5><?php print render($content['field_detail_caption']); ?></h5>
+	
+				<p>
+					<?php print render($content['field_detail_info']); ?>
+				</p>
+	
+			</div><!--/ .detail-entry-->
+	
+			<div data-color-state="<?php if (render($content['field_detail_color'])): ?>#<?php print render($content['field_detail_color']); ?> <?php endif; ?>" data-color-hover="" class="transform"></div>
+	
+		</div><!--/ .detail-box-->
+  </a>
 </div><!--/ .columns-->
