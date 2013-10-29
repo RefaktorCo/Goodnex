82296169c012dc0d5687a4752d676781ac2f91a9
refaktorco <refaktor.co@gmail.com>
2013-10-29 12:55:50 -0700

    -Missing drupal links in node--article

--- templates/node/node--article.tpl.php
+++ templates/node/node--article.tpl.php
@@ -149,6 +149,21 @@ $counter = count($items);
 	</div><!--/ .about-author-->
 	<?php endif;?>
   </div>
+  
+  <?php
+    // Remove the "Add new comment" link on the teaser page or if the comment
+    // form is being displayed on the same page.
+    if ($teaser || !empty($content['comments']['comment_form'])) {
+      unset($content['links']['comment']['#links']['comment-add']);
+    }
+    // Only display the wrapper div if there are links.
+    $links = render($content['links']);
+    if ($links):
+  ?>
+    <div class="link-wrapper">
+      <?php print $links; ?>
+    </div>
+  <?php endif; ?>
 
   <?php print render($content['comments']); ?>
 </article>
