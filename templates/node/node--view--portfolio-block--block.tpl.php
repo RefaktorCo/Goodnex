b59a58ff3868a329ea2829c904683f4da0b76a81
refaktorco <refaktor.co@gmail.com>
2013-10-29 16:41:20 -0700

    -did more of those file_create fixes, this should do it for the update.

--- templates/node/node--view--portfolio-block--block.tpl.php
+++ templates/node/node--view--portfolio-block--block.tpl.php
@@ -7,11 +7,13 @@ $replacement = " ";
 
 <article class="<?php echo theme_get_setting('portfolio_columns');?>" data-categories="<?php print strip_tags(render($content['field_portfolio_category'])); ?>">
 
+  <?php if (render($content['field_image'])): ?>  
 	<div class="preloader">
 		<a class="bwWrapper single-image plus-icon" href="<?php echo file_create_url($node->field_image['und'][0]['uri']); ?>" rel="folio">
 			<img src="<?php echo file_create_url($node->field_image['und'][0]['uri']); ?>" alt="" >
 		</a>							
 	</div><!--/ .preloader-->
+	<?php endif; ?>
 
 	<a class="project-meta" href="<?php echo $node_url; ?>">
 
